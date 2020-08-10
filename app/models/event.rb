10025ec964f5d28a1ad53dcc86171dff76384d71
Meyanis95 <b00694485@essec.edu>
2020-08-10 15:20:26 +0200

    Premier commit

---
+++ app/models/event.rb
@@ -0,0 +1,10 @@
+class Event < ApplicationRecord
+  has_many :users, through: attendance
+
+  validates :start_date, presence: true
+  validates :duration, presence: true
+  validates :title, presence: true, length: { in: 5..140 } 
+  validates :description, presence: true, length: { in: 20..1000 } 
+  validates :price, presence: true, length: { in: 1..1000 } 
+  validates :location, presence: true, length: { in: 1..1000 } 
+end
